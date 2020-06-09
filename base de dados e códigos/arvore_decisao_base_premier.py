9a68fb642377883e9236c65f56ee07b25fcd59ae
Andre Oliveira <aoliveira371@gmail.com>
2020-06-09 00:21:37 -0300

    acuracia pronta da tabela simples da premier league

--- base de dados e códigos/arvore_decisao_base_premier.py
+++ base de dados e códigos/arvore_decisao_base_premier.py
@@ -7,7 +7,8 @@ Created on Thu Jun  4 15:27:40 2020
 
 import pandas as pd
 
-base = pd.read_csv('dados_durante_jogo_pre_processados.csv')
+base_completa = pd.read_csv('dados_durante_jogo_pre_processados.csv')
+base = base_completa.copy()
 
 base.drop(columns=['pais','campeonato','temporada','golscasaft','golsvisitanteft',
                    'resultadoht','resultadoft','totalgolsft','possebolacasaft',
@@ -23,17 +24,20 @@ base.drop(columns=['pais','campeonato','temporada','golscasaft','golsvisitanteft
 
 previsores = base.iloc[:, 0:32].values
 classe = base.iloc[:, 32].values
-                
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+#from sklearn.preprocessing import LabelEncoder
+#labelencoder_X = LabelEncoder()
+#previsores[:, 0] = labelencoder_X.fit_transform(previsores[:, 0])
+#previsores[:, 1] = labelencoder_X.fit_transform(previsores[:, 1])
+#previsores[:, 31] = labelencoder_X.fit_transform(previsores[:, 31])
+
+#labelencoder_classe = LabelEncoder()
+#classe = labelencoder_classe.fit_transform(classe)
+
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
-
-labelencoder_X = LabelEncoder()
-previsores[:, 0] = labelencoder_X.fit_transform(previsores[:, 0])
-previsores[:, 1] = labelencoder_X.fit_transform(previsores[:, 1])
-previsores[:, 31] = labelencoder_X.fit_transform(previsores[:, 31])
-
-labelencoder_classe = LabelEncoder()
-classe = labelencoder_classe.fit_transform(classe)
+column_tranformer = ColumnTransformer([('one_hot_encoder', OneHotEncoder(), [0, 1, 31])],remainder='passthrough')
+previsores = column_tranformer.fit_transform(previsores)
 
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
@@ -48,5 +52,5 @@ classificador.fit(previsores_treinamento, classe_treinamento)
 previsoes = classificador.predict(previsores_teste)
 
 from sklearn.metrics import confusion_matrix, accuracy_score
-precisao = accuracy_score(classe_teste, previsoes)
+acuracia = accuracy_score(classe_teste, previsoes)
 matriz = confusion_matrix(classe_teste, previsoes)
